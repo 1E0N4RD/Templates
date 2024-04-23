468a10f1ec821f8165b15a2fa09116950f0e856f
1E0N4RD <welsch.leonard@protonmail.com>
2024-04-23 12:06:58 +0200

    Added Zig flake

---
+++ flakes/zig-dev/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.zig_0_12
+        pkgs.fish
+      ];
+
+      shellHook = ''
+        fish
+      '';
+    };
+  };
+
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+}
