9be58565b7b2e3c2ba834baee1efa55cd12c88bc
1E0N4RD <welsch.leonard@protonmail.com>
2024-04-27 22:11:54 +0200

    Added flake for developing with the gnu toolchain

---
+++ flakes/gnu-dev/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake for developing with the gnu toolchain";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        gcc
+        gnumake
+        fish
+      ];
+
+      shellHook = ''
+        exec fish
+      '';
+    };
+  };
+
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+}
